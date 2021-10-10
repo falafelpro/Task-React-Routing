5bdf0c0e793f294a515568381afec1a773b337d4
falafelpro <47983933+falafelpro@users.noreply.github.com>
2021-10-10 17:53:16 +0300

    Nawaf's First  push

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import { Route, Switch } from "react-router";
+import NavBar from "./components/NavBar";
+
 import Home from "./components/Home";
 // Components
 import ProductList from "./components/ProductList";
 //styling
-import "./App.css"
-import Detail from './components/Detail'
-
+import "./App.css";
+import Detail from "./components/Detail";
 
 function App() {
-
   return (
-    <div >
-      <Home />
-      <ProductList />
-      <Detail />
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/productList">
+          <ProductList />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
